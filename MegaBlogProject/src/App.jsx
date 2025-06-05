aebdc3c714ffff65f9a436ed6b78ba17567c0961
priyanshu-369 <priyanshugupta15993097@gmail.com>
2025-06-05 22:02:29 +0530

    chore: update project configuration files for better organization

--- MegaBlogProject/src/App.jsx
+++ MegaBlogProject/src/App.jsx
@@ -1,33 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className='w-full bg-gray-700 py-8'>
+          <div className='w-xl mx-auto bg-gray-200 h-80 rounded-lg flex justify-center items-center px-5 border-white border-2'>
+            <h1 className='text-2xl text-gray-700  font-bold font-serif'>
+              Billa lets build the best project that can be build and deploy it...😎
+            </h1>
+          </div>
+        </div>
     </>
   )
 }
